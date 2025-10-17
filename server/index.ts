ef127f853c6df5422fac19c63aa041211cbbdcec
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2025-10-17 16:42:27 +0600

    update package and index

--- server/index.ts
+++ server/index.ts
@@ -82,9 +82,9 @@ app.use((req, res, next) => {
   server.listen(
     {
       port,
-      // host: "0.0.0.0",
-      host: "localhost",
-      // reusePort: true,
+      host: "0.0.0.0",
+      //host: "localhost",
+      reusePort: true,
     },
     () => {
       log(`serving on port ${port}`);
